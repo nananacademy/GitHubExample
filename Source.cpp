52766dc734276942eb7e95c0c28a2f1541fdc9ac
Reid G <reid.m.gillis@gmail.com>
2026-07-08 17:34:25 -0700

    initial commit

---
+++ Source.cpp
@@ -0,0 +1,11 @@
+int main()
+{
+	int myInt = 3; //staged
+	float myFloat = 4.0f; //staged
+	float myFloatA = 10.05f;
+	float myFloatB = 41.5f;
+
+	bool myBool = false;
+
+	bool myOtherBool = true;
+}
