6b81427a10249a0e2a6f39ab933acfaca1d29788
Reid G <reid.m.gillis@gmail.com>
2026-07-08 19:50:09 -0700

    removed myFloatA and changed myFoatB

--- Source.cpp
+++ Source.cpp
@@ -2,8 +2,8 @@ int main()
 {
 	int myInt = 3; //staged
 	float myFloat = 4.0f; //staged
-	float myFloatA = 10.05f;
-	float myFloatB = 41.5f;
+	
+	float myFloatB = 166.5f;
 
 	bool myBool = false;
 }
