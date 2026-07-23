cb6b541f082f594cf0583976caa807dd36331e6b
Reid G <reid.m.gillis@gmail.com>
2026-07-22 21:56:53 -0700

    delete Utilities

--- Utlilities.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <string>
-#include <iostream>
-
-#define PRINT(varToPrint) std::cout << varToPrint
-#define EMPTY std::cout << "" << std::endl
-
-using String = std::string;
-
