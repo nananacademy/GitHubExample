1d286752369243c805f9fe6b61281695c0c4ea64
Reid G <reid.m.gillis@gmail.com>
2026-07-22 21:41:58 -0700

    DamageToApply change to 1000

--- Source.cpp
+++ Source.cpp
@@ -9,7 +9,7 @@ int FeatureToAdd(int InValueA, int InValueB)
 
 int main()
 {
-	int DamageToApply = 9000;
+	int DamageToApply = 1000;
 	Add(10);
 	String myOtherString = "my name";
 	String myString = "hello world";
