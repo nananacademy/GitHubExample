70994ab9201cf89af65aaf2c94dfa58674be83a3
Reid G <reid.m.gillis@gmail.com>
2026-07-30 14:09:00 -0700

    Change MainMenu to singleton pattern. Add DisplateDamage()

--- MainMenu.cpp
+++ MainMenu.cpp
@@ -1,7 +1,27 @@
 #include "MainMenu.h"
 #include <iostream>
 
+MainMenu* MainMenu::mInstance = nullptr;
+
+MainMenu::MainMenu()
+{
+}
+
+MainMenu* MainMenu::GetInstance()
+{
+	if (mInstance == nullptr)
+	{
+		mInstance = new MainMenu();
+	}
+	return mInstance;
+}
+
 void MainMenu::DisplayMenu()
 {
 	std::cout << "main menu" << std::endl;
+}
+
+void MainMenu::DisplayDamage(int InDamage)
+{
+	std::cout << "damage " << InDamage << std::endl;
 }
