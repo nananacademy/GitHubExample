acb75555b0f276ef95bd70fd41db205f15d83bc1
Reid G <reid.m.gillis@gmail.com>
2026-07-31 18:22:05 -0700

    Add Getter Functtions

--- Environment.h
+++ Environment.h
@@ -6,8 +6,19 @@ class Environment
 	std::string mName = "default";
 	char mIcon = '=';
 
+
 public:
 	virtual void Interact();
 	virtual void OnOverlap();
+
+	std::string GetName()
+	{
+		mName;
+	}
+
+	char GetIcon()
+	{
+		return mIcon;
+	}
 };
 
